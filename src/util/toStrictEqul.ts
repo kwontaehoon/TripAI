c66baaf64124c069d31af944aed6d783c549c54e
kwontaehoon <gju04195@gmail.com>
2025-02-15 15:52:25 +0900

    feat: Math.random, each test

--- src/util/toStrictEqul.ts
+++ src/util/toStrictEqul.ts
@@ -1,3 +1,6 @@
 export const obj = () => {
     return { a: "hello"};
 }
+export const random = () => {
+    return { a: Math.random() };
+}
--- test/toStrictEqul.spec.ts
+++ test/toStrictEqul.spec.ts
@@ -1,6 +1,6 @@
 import { sum } from "../src/util/toBe"
 import { test, expect } from '@jest/globals'
-import { obj } from "../src/util/toStrictEqul"
+import { obj, random } from "../src/util/toStrictEqul"
 
 // 첫 번째 인자는 설명
 // 두 번째 인자는 테스트 함수       
@@ -14,4 +14,9 @@ test("배열끼리도 toStrictEqual 써야한다.", () => {
     expect([1,2,3]).not.toBe([1,2,3]);
     
     expect(obj()).toStrictEqual({ a: "hello"});
-}); 
+});
+
+// Math.random()
+test("배열끼리도 toStrictEqual 써야한다.", () => {
+    expect(random()).toStrictEqual({ a: expect.any(Number) });
+});
