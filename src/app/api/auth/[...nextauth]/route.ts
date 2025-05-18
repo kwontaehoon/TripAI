44752e87ee51a9911666f81590af4a70e86a091e
kwontaehoon <gju04195@gmail.com>
2025-05-18 23:24:52 +0900

    path: route.ts에 authOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,5 @@
-// app/api/auth/[...nextauth]/route.ts
-
-import NextAuth from "next-auth"
-import KakaoProvider from "next-auth/providers/kakao"
-import GoogleProvider from "next-auth/providers/google"
-import NaverProvider from "next-auth/providers/naver"
-import { AuthOptions } from "next-auth";
-
-export const authOptions: AuthOptions = {
-  providers: [
-    KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID!,
-      clientSecret: process.env.KAKAO_CLIENT_SECRET || "",
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-    }),
-    NaverProvider({
-      clientId: process.env.NAVER_CLIENT_ID!,
-      clientSecret: process.env.NAVER_CLIENT_SECRET || ""
-    })
-  ],
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/authOptions";
 
 const handler = NextAuth(authOptions);
 
