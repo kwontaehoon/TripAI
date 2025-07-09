23a530ceb9e89883d864f6482e2a669737c21c30
kwontaehoon <gju04195@gmail.com>
2025-07-09 15:41:55 +0900

    config: images patterns

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [new URL('https://tvkqolkaaqmqftrawadd.supabase.co/storage/v1/object/public/trip-ai/**')],
+  },
 };
 
 export default nextConfig;
