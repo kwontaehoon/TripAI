924fedad516fff129ac353685d1fb59d0a0a3b03
kwontaehoon <gju04195@gmail.com>
2025-07-10 00:43:41 +0900

    fix: cleanJson

--- src/util/cleanJson.ts
+++ src/util/cleanJson.ts
@@ -5,5 +5,6 @@ export const cleanJson = (jsonString:string) => {
         .replace(/\/\*[\s\S]*?\*\//g, "") // /* */ 주석 제거
         .replace(/,(\s*})/g, "$1")  // 객체 마지막 쉼표 제거
         .replace(/,(\s*\])/g, "$1") // 배열 마지막 쉼표 제거
+        .replace(/[\u0000-\u001F]+/g, " ")   // 제어문자 (ASCII 0~31) 제거
         .trim();
 }
