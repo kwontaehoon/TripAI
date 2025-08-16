1c9cc0eae202af156c7b46d61ef406eaa56ac0ed
kwontaehoon <gju04195@gmail.com>
2025-08-16 23:31:57 +0900

    config:  auth/새로고침 마다 userInfo 요청 해제

--- src/config/provider/authProvider.tsx
+++ src/config/provider/authProvider.tsx
@@ -10,10 +10,10 @@ import { useUserInfoMutation } from '@/hooks/supabase/dev';
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   const supabase = createClient();
-  const { mutate: userInfo } = useUserInfoMutation()
+  // const { mutate: userInfo } = useUserInfoMutation()
 
   const setSession = useSetAtom(sessionAtom)
-  const setUserInfo = useSetAtom(userInfoAtom)
+  // const setUserInfo = useSetAtom(userInfoAtom)
 
   useEffect(() => {
     // supabase.auth.getSession().then(({ data: { session } }) => {
@@ -26,11 +26,13 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     } = supabase.auth.onAuthStateChange((_event, newSession) => {
       setSession(newSession)
 
-      userInfo(newSession?.user.email, {
-        onSuccess: (userInfo) => {
-          setUserInfo(userInfo)
-        }
-      })
+      // if(newSession){
+      //   userInfo(newSession?.user.email, {
+      //     onSuccess: (userInfo) => {
+      //       setUserInfo(userInfo)
+      //     }
+      //   })
+      // }
 
       if (_event === 'SIGNED_IN') {
         console.log('SIGNED_IN');
