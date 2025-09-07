3ba7f3b7f940ef7fadea47f20d939c226a832d6a
kwontaehoon <gju04195@gmail.com>
2025-09-07 19:32:21 +0900

    refactor: comma util 띄어쓰기

--- src/util/comma.ts
+++ src/util/comma.ts
@@ -1,5 +1,5 @@
 export const comma = (data:number, priceFlag) => {
     if(priceFlag){
-        return `₩${String(data).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+        return `₩ ${String(data).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
     }else return `${String(data).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
 }
