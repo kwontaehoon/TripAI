378b0d9e0def7f0eb6b7c150dd3e884867ca50c0
kwontaehoon <gju04195@gmail.com>
2025-09-10 00:14:19 +0900

    config: env supabase key name 수정

--- src/service/supabase/middleware.ts
+++ src/service/supabase/middleware.ts
@@ -1,6 +1,5 @@
 import { createServerClient } from "@supabase/ssr"
 import { NextResponse, type NextRequest } from "next/server"
-import { getUserInfo } from "."
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
@@ -9,7 +8,7 @@ export async function updateSession(request: NextRequest) {
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
       cookies: {
         getAll() {
@@ -82,7 +81,7 @@ export async function updateSession(request: NextRequest) {
       const { error: view_history_insertError } = await supabase
         .from("view_history")
         .insert({
-          user_id: user?.id || null,
+          user_id: user?.id,
           board_id: Number(boardId),
         })
 
