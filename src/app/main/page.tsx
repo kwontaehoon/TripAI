7a3c97aa958ebd04995ce1108fae1f5d4af45f8c
kwontaehoon <gju04195@gmail.com>
2026-03-11 17:22:37 +0900

    refactor: main 페이지 prefetch 로직 분리 및 최적화

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,7 +1,6 @@
 import { Camera, Mountain } from "lucide-react"
-import Left from "./left/page"
-import Right from "./right/page"
-import Intro from "./Intro"
+import LeftPanel from "./left/leftPanel"
+import RightPanel from "./right/rightPanel"
 import {
   getBoards,
   getBoardsInfinite,
@@ -12,65 +11,29 @@ import {
 } from "@/service/supabase"
 import { dehydrate, QueryClient } from "@tanstack/react-query"
 import { Hydration } from "../Hydration"
-// import { useSession } from "next-auth/react"
-// import { queryClient } from "@/config/provider/queryClientProvider"
-// import { Metadata, ResolvingMetadata } from "next"
-
+import {
+  prefetchBoards,
+  prefetchBoardsInfinite,
+  prefetchCourses,
+  prefetchCoursesAndBoards,
+  prefetchCoursesAndBoardsGallery,
+  prefetchCoursesInfinite,
+  prefetchPopularLocation,
+} from "@/service/prefetch"
 export const revalidate = 3600
 
 const Page = async () => {
-  const [
-    galleryList,
-    popularLocation,
-    boardsInfiniteData,
-    coursesInfiniteData,
-    boardsData,
-    coursesData,
-  ] = await Promise.all([
-    getCoursesAndBoardsGallery(),
-    getPopularLocation(),
-    getBoardsInfinite({ pageParam: "1" }),
-    getCoursesInfinite({ pageParam: "1" }),
-    getBoards(),
-    getCourses(),
-  ])
-
   const queryClient = new QueryClient()
 
-  queryClient.setQueryData(["boards", "infinite"], {
-    pages: [boardsInfiniteData],
-    pageParams: [null],
-  })
-  queryClient.setQueryData(["courses", "infinite"], {
-    pages: [coursesInfiniteData],
-    pageParams: [null],
-  })
-
-  // await Promise.all(
-  //   [1, 2, 3, 4].map((id) => prefetchCourseDetails(queryClient, id)),
-  // )
-  // await Promise.all(
-  //   [1, 3, 6, 63].map((id) => prefetchBoardDetails(queryClient, id)),
-  // )
-
-  // const { data: pageNationData, isLoading, isFetching } = useTestPageNationQuery(state)
-
-  // useEffect(() => {
-  //   console.log("isLoading: ", isLoading, isFetching)
-  // }, [pageNationData])
-
-  // const { data: session } = useSession()
-  // console.log(session)
-
-  // if (session) {
-  //   return (
-  //     <div className='py-28'>
-  //       <p>{session.user?.name} 님 환영합니다!</p>
-  //       <div>123</div>
-  //       <button onClick={() => signOut()}>로그아웃</button>
-  //     </div>
-  //   );
-  // }
+  // prefech
+  await Promise.all([
+    prefetchPopularLocation(queryClient),
+    prefetchCourses(queryClient),
+    prefetchCoursesInfinite(queryClient, null),
+    prefetchBoards(queryClient),
+    prefetchBoardsInfinite(queryClient, null),
+    prefetchCoursesAndBoardsGallery(queryClient),
+  ])
 
   return (
     <Hydration state={dehydrate(queryClient)}>
@@ -81,65 +44,23 @@ const Page = async () => {
         px-4 lg:px-0 py-28 mb-4 space-x-0
         lg:flex lg:mb-12 lg:space-x-12"
       >
-        {/* <div className="py-28">
-        <button onClick={() => signIn("kakao", { callbackUrl: "/" })}>
-          카카오 로그인
-        </button>
-        <br />
-        <button onClick={() => signIn("google", { callbackUrl: "/" })}>
-          구글 로그인
-        </button>
-        <br />
-        <button onClick={() => signIn("naver", { callbackUrl: "/" })}>
-          네이버 로그인
-        </button>
-      </div> */}
-
-        <Intro />
-        <Left
-          galleryList={galleryList}
-          popularLocation={popularLocation}
-          boardsInfiniteData={boardsInfiniteData?.boards}
-          coursesInfiniteData={coursesInfiniteData?.courses}
-        />
-        <Right boardsData={boardsData} coursesData={coursesData} />
+        <LeftPanel />
+        <RightPanel />
 
         {/* Floating Elements */}
-        <div
-          className="absolute hidden 2xl:block top-32 left-20 animate-bounce"
-          data-oid="44lb0:h"
-        >
-          <div
-            className="bg-white rounded-full p-4 shadow-lg"
-            data-oid=".wyh69n"
-          >
-            <Camera className="h-6 w-6 text-blue-600" data-oid="v._bw-7" />
+        <div className="absolute hidden 2xl:block top-32 left-20 animate-bounce">
+          <div className="bg-white rounded-full p-4 shadow-lg">
+            <Camera className="h-6 w-6 text-blue-600" />
           </div>
         </div>
-        <div
-          className="absolute hidden 2xl:block top-32 right-20 animate-bounce delay-1000"
-          data-oid="w62gl6y"
-        >
-          <div
-            className="bg-white rounded-full p-4 shadow-lg"
-            data-oid="sd40lp9"
-          >
-            <Mountain className="h-6 w-6 text-green-600" data-oid="-kbcjph" />
+        <div className="absolute hidden 2xl:block top-32 right-20 animate-bounce delay-1000">
+          <div className="bg-white rounded-full p-4 shadow-lg">
+            <Mountain className="h-6 w-6 text-green-600" />
           </div>
         </div>
       </div>
     </Hydration>
   )
-
-  // return (
-  //   <div className='py-28'>
-  //     <button onClick={() => signIn("kakao", { callbackUrl: "/" })}>카카오 로그인</button>
-  //     <br />
-  //     <button onClick={() => signIn("google", { callbackUrl: "/" })}>구글 로그인</button>
-  //     <br />
-  //     <button onClick={() => signIn("naver", { callbackUrl: "/" })}>네이버 로그인</button>
-  //   </div>
-  // );
 }
 
 export default Page
