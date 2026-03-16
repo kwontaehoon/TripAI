605ac09186ae34a81e9bd2a8a4fc0e3596b19e78
kwontaehoon <gju04195@gmail.com>
2026-03-16 22:45:37 +0900

    chore: remove commented SessionProvider code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import MainPage from '@/app/main/page'
-// import { SessionProvider } from 'next-auth/react'
 
 const page = () => {
-
   return (
     <div>
-      {/* <SessionProvider> */}
       <MainPage />
-      {/* </SessionProvider> */}
     </div>
   )
 }
